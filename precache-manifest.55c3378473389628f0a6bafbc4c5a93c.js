920ca8e12626acc2f2980294f61d6efdd1c13be9
Lokprakash-babu <lokprakash100@gmail.com>
2020-08-16 07:16:16 +0530

    Updates

---
+++ precache-manifest.55c3378473389628f0a6bafbc4c5a93c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56234579deb0907068dfafa98de4885",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "e9f4d3c57f6182ba78c1",
+    "url": "/monsters/static/css/main.ba2127bb.chunk.css"
+  },
+  {
+    "revision": "e26f3b49cda25c8c1e51",
+    "url": "/monsters/static/js/2.36960f10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.36960f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f4d3c57f6182ba78c1",
+    "url": "/monsters/static/js/main.432e9f2f.chunk.js"
+  },
+  {
+    "revision": "0b71497f43c75273b692",
+    "url": "/monsters/static/js/runtime-main.da15c421.js"
+  }
+]);
